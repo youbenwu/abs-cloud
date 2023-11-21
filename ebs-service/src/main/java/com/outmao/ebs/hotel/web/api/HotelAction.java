4017af72239441c1a22946d569c6434826e7ff93
youbenwu <youbenwu@live.com>
2023-11-21 13:04:21 +0800

    ebs

--- ebs-service/src/main/java/com/outmao/ebs/hotel/web/api/HotelAction.java
+++ ebs-service/src/main/java/com/outmao/ebs/hotel/web/api/HotelAction.java
@@ -12,10 +12,12 @@ import com.outmao.ebs.hotel.vo.*;
 import com.outmao.ebs.qrCode.dto.GenerateQrCodeDTO;
 import com.outmao.ebs.qrCode.service.QrCodeService;
 import com.outmao.ebs.security.util.SecurityUtil;
+import com.outmao.ebs.security.vo.SecurityUser;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,7 +42,12 @@ public class HotelAction {
     @ApiOperation(value = "注册酒店", notes = "注册酒店")
     @PostMapping("/register")
     public void registerHotel(@RequestBody RegisterHotelDTO request) {
-         hotelService.registerHotel(request);
+        hotelService.registerHotel(request);
+        try{
+            SecurityContextHolder.getContext().getAuthentication().setAuthenticated(false);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 
